3c8865f5c3601912ed67e187207db2703fcd84b9
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-12-16 18:04:45 -0300

    refactor: recria firebaseConfig utilizando variáveis de ambiente

--- src/api/api.ts
+++ src/api/api.ts
@@ -11,9 +11,11 @@ import {
   onSnapshot,
   getDoc,
 } from "firebase/firestore";
-import { firebaseConfig } from "../firebaseConfig";
 import type { User, UserChat } from "../types/User";
 import type { Dispatch, SetStateAction } from "react";
+import { firebaseConfig } from "../firebaseConfig.ts";
+
+console.log("firebaseConfig: ", firebaseConfig);
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
